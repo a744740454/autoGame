bcfd60fe4dbe993d60acd0ada204d0a28a611156
施意波 <2322540522@qq.com>
2024-12-23 13:58:58 +0800

    fix bug

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import yaml
 import pymysql
 from flask import Flask
 from models import db
+
 # Install pymysql as MySQLdb
 pymysql.install_as_MySQLdb()
+
 
 # Initialize the Flask app and the database
 
@@ -12,11 +14,13 @@ def load_config(config_path='config/prod.yaml'):
         config = yaml.safe_load(file)
     return config
 
+
 def get_mysql_uri(config):
     mysql_config = config['mysql']
     url = f"mysql://{mysql_config['username']}:{mysql_config['password']}@{mysql_config['host']}:{mysql_config['port']}/{mysql_config['database']}"
     print(url)
     return url
+
 
 def create_app(config):
     app = Flask(__name__)
